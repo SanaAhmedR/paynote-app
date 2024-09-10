112e8c2a62a2d4a3c6774c27e7269acad5efa004
HotProspector <139770095+SanaAhmedR@users.noreply.github.com>
2024-09-10 10:36:17 +0500

    Update paynote.js

--- src/paynote.js
+++ src/paynote.js
@@ -1,64 +1,64 @@
+
+/* eslint-disable no-undef */
+
 import React, { useEffect } from "react";
 
-const PaynoteButton = ({
-  customerEmail,
-  firstName,
-  lastName,
-  widgetSelector,
-}) => {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src =
-      "https://developers.seamlesschex.com/docs/checkoutjs/sdk-min.js";
-    script.async = true;
-    script.defer = true;
-    script.onload = () => {
-      try {
-        const objRequestIframe = {
-          publicKey: "pk_test_01H8PVPA6Y7T7TBHPNJHYHKZ2C",
-          sandbox: true,
-          displayMethod: "iframe",
-          paymentToken: "pay_tok_SPECIMEN-" + Math.random(),
-          widgetContainerSelector: widgetSelector,
-          saveBankDetails: true,
-          storeName: "AlphaBetaGamma",
-          checkout: {
-            totalValue: 3,
-            currency: "USD",
-            description: "Sign up to System",
-            items: [{ title: "Enrollment", price: 3 }],
-            customerEmail: customerEmail,
-            customerFirstName: firstName,
-            customerLastName: lastName,
-          },
-          style: {
-            buttonClass: "btn green-btn btn-block no-overflow",
-            buttonColor: "#00b660",
-            buttonLabelColor: "#ffffff",
-            buttonLabel: "Pay",
-          },
-          onSuccess: function () {
-            console.log("Payment successful!");
-          },
-          onError: function () {
-            console.log("Error during payment");
-          },
-        };
-        const paynoteIframe = new window.PAYNOTE(objRequestIframe);
-        paynoteIframe.render();
-      } catch (error) {
-        alert("Error initializing PAYNOTE: " + error.message);
-      }
-    };
+const PaynoteButton = (customerEmail, firstName, lastName, widgetSelector) => {
+useEffect(() => {
+const script = document.createElement("script");
+script.src =
+"https://developers.seamlesschex.com/docs/checkoutjs/sdk-min.js";
+script.async = true;
+script.defer = true;
+script.onload = () => {
+try {
+const objRequestIframe = {
+publicKey: "pk_test_01H8PVPA6Y7T7TBHPNJHYHKZ2C",
+sandbox: true,
+// authorizationOnly: true, // set this to true
+displayMethod: "iframe",
+paymentToken: "pay_tok_SPECIMEN-" + Math.random(),
+widgetContainerSelector: widgetSelector,
+saveBankDetails: true,
+storeName: "AlphaBetaGamma",
+checkout: {
+totalValue: 3,
+currency: "USD",
+description: "Sign up to System",
+items: [{ title: "Enrollment", price: 3 }],
+customerEmail: customerEmail,
+customerFirstName: firstName,
+customerLastName: lastName,
+},
+style: {
+buttonClass: "btn green-btn btn-block no-overflow",
+buttonColor: "#0091D5",
+buttonLabelColor: "#ffffff",
+buttonLabel: "Add Account Details",
+},
+onSuccess: function () {
+console.log("Payment successful!");
+},
+onError: function () {
+console.log("Errrrrrrrrrorrrroorororoorororo");
+},
+};
+const paynoteIframe = new PAYNOTE(objRequestIframe);
+paynoteIframe.render();
+} catch (error) {
+alert("Error initializing PAYNOTE:", error);
+}
+};
 
-    document.body.appendChild(script);
+document.body.appendChild(script);
 
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, [customerEmail, firstName, lastName, widgetSelector]);
+return () => {
+document.body.removeChild(script);
+};
+}, []);
 
-  return <div className={`${widgetSelector}`}></div>;
+return <div className={`${widgetSelector}`}>Hello World</div>;
+
 };
 
 export default PaynoteButton;
